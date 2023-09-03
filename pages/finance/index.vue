1fa03cea7826e090ab75d7f84c05d28d93ffae91
KenichiOkamuraJp <kenichi.okamura.jp@gmail.com>
2023-09-03 21:51:20 +0900

    20230903

--- pages/finance/index.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <NuxtLink to="/">トップに戻る</NuxtLink>
-  <a href="https://data.polyarchy.net">外部サイト</a>
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <NuxtLink to="/finance">企業財務情報</NuxtLink>
+  <h2>Polyarchy.net</h2>
+  <ol>
+    <li><a href="https://data.polyarchy.net/finance" target="_blank">企業財務情報</a></li>
+    <li>政府会議体情報</li>
+    <li>基本統計情報</li>
+    <li>地方議会情報</li>
+  </ol>
 </template>
