dba4eccb2eb28d9a6db75d06305c9ca5311a7dd2
KenichiOkamuraJp <kenichi.okamura.jp@gmail.com>
2023-09-17 10:56:18 +0900

    20230917

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <h2>Polyarchy.net</h2>
   <ol>
     <li><a href="https://data.polyarchy.net/finance" target="_blank">企業財務情報</a></li>
-    <li>政府会議体情報</li>
+    <li><a href="https://data.polyarchy.net/government-meetings" target="_blank">政府会議体情報</a></li>
     <li>基本統計情報</li>
     <li>地方議会情報</li>
   </ol>
